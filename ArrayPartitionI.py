774fbd2c71491303bba1502ce761d7e01f417781
MingYanWoo <woomingyan@gmail.com>
2018-05-26 21:03:15 +0800

    normal commit

--- ArrayPartitionI.py
+++ ArrayPartitionI.py
@@ -4,5 +4,6 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+    
         nums.sort()
         return sum([nums[i] for i in range(len(nums)) if i % 2 == 0])
