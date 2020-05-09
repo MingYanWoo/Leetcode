53def7130401e586b6ff697376e772b5b88f189f
MingYanWoo <woomingyan@gmail.com>
2020-05-09 15:07:04 +0800

    Create 215. 数组中的第K个最大元素.py

---
+++ 215. 数组中的第K个最大元素.py
@@ -0,0 +1,38 @@
+# 215. 数组中的第K个最大元素
+# 在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+
+# 示例 1:
+
+# 输入: [3,2,1,5,6,4] 和 k = 2
+# 输出: 5
+# 示例 2:
+
+# 输入: [3,2,3,1,2,4,5,5,6] 和 k = 4
+# 输出: 4
+# 说明:
+
+# 你可以假设 k 总是有效的，且 1 ≤ k ≤ 数组的长度。
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        start, end = 0, len(nums)-1
+        mid = self.partition(nums, start, end)
+        while mid != end-k+1:
+            if mid > end-k+1:
+                mid = self.partition(nums, start, mid-1)
+            else:
+                mid = self.partition(nums, mid+1, end)
+        return nums[mid]
+
+    def partition(self, nums, start, end):
+        midVal = nums[end]
+        while start < end:
+            while nums[start] <= midVal and start < end:
+                start += 1
+            nums[end] = nums[start]
+            while nums[end] > midVal and start < end:
+                end -= 1
+            nums[start] = nums[end]
+        nums[start] = midVal
+        return start
