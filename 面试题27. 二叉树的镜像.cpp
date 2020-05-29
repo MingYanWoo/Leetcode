033757d4dcfc93b296a07c4f4fe6c8a0f28bcb0e
MingYanWoo <woomingyan@gmail.com>
2020-05-29 17:06:52 +0800

    Create 面试题27. 二叉树的镜像.cpp

---
+++ 面试题27. 二叉树的镜像.cpp
@@ -0,0 +1,47 @@
+// 面试题27. 二叉树的镜像
+// 请完成一个函数，输入一个二叉树，该函数输出它的镜像。
+
+// 例如输入：
+
+//      4
+//    /   \
+//   2     7
+//  / \   / \
+// 1   3 6   9
+// 镜像输出：
+
+//      4
+//    /   \
+//   7     2
+//  / \   / \
+// 9   6 3   1
+
+ 
+
+// 示例 1：
+
+// 输入：root = [4,2,7,1,3,6,9]
+// 输出：[4,7,2,9,6,3,1]
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mirrorTree(TreeNode* root) {
+        if(!root) return NULL;
+        TreeNode *left = mirrorTree(root->left);
+        TreeNode *right = mirrorTree(root->right);
+        root->right = left;
+        root->left = right;
+        return root;
+    }
+};
