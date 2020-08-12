287dda163de0c4f9aebf941d468e08efebb46510
MingYanWoo <woomingyan@gmail.com>
2020-08-12 11:08:38 +0800

    Update 面试题24. 反转链表.cpp

--- 面试题24. 反转链表.cpp
+++ 面试题24. 反转链表.cpp
@@ -38,3 +38,16 @@ public:
         return head;
     }
 };
+
+
+// 递归
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head || !head->next) return head;
+        ListNode *last = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return last;
+    }
+};
