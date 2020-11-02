077163a0de3796f0e69d546379020d1a16f5c710
MingYanWoo <woomingyan@gmail.com>
2020-11-02 21:09:03 +0800

    Create 剑指 Offer 32 - III. 从上到下打印二叉树 III.cpp

---
+++ 剑指 Offer 32 - III. 从上到下打印二叉树 III.cpp
@@ -0,0 +1,67 @@
+// 剑指 Offer 32 - III. 从上到下打印二叉树 III
+
+// 请实现一个函数按照之字形顺序打印二叉树，即第一行按照从左到右的顺序打印，第二层按照从右到左的顺序打印，第三行再按照从左到右的顺序打印，其他行以此类推。
+
+ 
+
+// 例如:
+// 给定二叉树: [3,9,20,null,null,15,7],
+
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
+// 返回其层次遍历结果：
+
+// [
+//   [3],
+//   [20,9],
+//   [15,7]
+// ]
+ 
+
+// 提示：
+
+// 节点总数 <= 1000
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        if(!root) return {};
+        queue<TreeNode*> q;
+        q.push(root);
+        vector<vector<int>> res;
+        bool odd = true;
+        while(!q.empty()) {
+            int n = q.size();
+            vector<int> temp;
+            for(int i = 0; i < n; i++) {
+                TreeNode *node = q.front();
+                q.pop();
+                temp.push_back(node->val);
+                if(node->left)
+                    q.push(node->left);
+                if(node->right)
+                    q.push(node->right);
+            }
+            if(!odd)
+                reverse(temp.begin(), temp.end());
+            odd = !odd;
+            res.push_back(temp);
+        }
+        return res;
+    }
+};
